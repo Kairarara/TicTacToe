95b40c2d97ecda4bfdf19bb17ff87cad644c16f7
Pierluigi Caruso <pierluigic97@gmail.com>
2020-02-09 17:08:07 +0100

    Updates

---
+++ precache-manifest.93f6c2df1ee60ab540007c699826e700.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1888b5568071fde1b74170ca9c1164d8",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "f554a62bfa7d2c84f2eb",
+    "url": "/TicTacToe/static/css/main.1895421e.chunk.css"
+  },
+  {
+    "revision": "0d25c1b6e61c5f192396",
+    "url": "/TicTacToe/static/js/2.9b7fa7bf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/TicTacToe/static/js/2.9b7fa7bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f554a62bfa7d2c84f2eb",
+    "url": "/TicTacToe/static/js/main.25145765.chunk.js"
+  },
+  {
+    "revision": "732ef5cc8e4be803865c",
+    "url": "/TicTacToe/static/js/runtime-main.f8ddd752.js"
+  },
+  {
+    "revision": "25060cc8213d1536afb1cb7c3ab3888e",
+    "url": "/TicTacToe/static/media/gameBase.25060cc8.svg"
+  },
+  {
+    "revision": "423b9eaedaf254f4c0e4cd5985dc254f",
+    "url": "/TicTacToe/static/media/notebook_page.423b9eae.jpg"
+  }
+]);
